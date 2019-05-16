d07b6151213b677b6d9a50b9e0ea7f62386ea8bd
Chenbo Yi <yichenbo@outlook.com>
2019-05-16 15:40:45 +0100

    update the notelist view to contain href link

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,24 +1,49 @@
 (function(exports){
-  function testNoteListView(){
-    list = ['Food: pesto','Drink: seltzer']
-    var display = '<ul><li><div>Food: pesto</div></li><li><div>Drink: seltzer</div></li></ul>';
+  function testNoteListViewWithUrl(){
 
-    var noteListView = new NoteListView(list);
+     function Notedouble0(){};
+     Notedouble0.prototype.getText = function(){return "Food: pesto"};
+     Notedouble0.prototype.getId = function(){return 0};
+     note0 = new Notedouble0();
+
+     function Notedouble1(){};
+     Notedouble1.prototype.getText = function(){return "Drink: seltzer"};
+     Notedouble1.prototype.getId = function(){return 1};
+     note1 = new Notedouble1();
+
+     notelist = [note0, note1]
+
+    var display = '<ul><li><div><a herf="http://localhost:8080#notes/0">Food: pesto</a></div></li><li><div><a herf="http://localhost:8080#notes/1">Drink: seltzer</a></div></li></ul>';
+    var noteListView = new NoteListView(notelist);
+
     assert.isTrue(noteListView.get_view() === display)
   }
-    testNoteListView()
-    exports.testNoteListView = testNoteListView;
+    testNoteListViewWithUrl()
+    exports.testNoteListViewWithUrl = testNoteListViewWithUrl;
 })(this);
 
 
 (function(exports){
-  function testLimitLengthView(){
-    list = ['Favourite food: pesto','Favourite drink: seltzer']
-    var display = '<ul><li><div>Favourite food: pest</div></li><li><div>Favourite drink: sel</div></li></ul>';
+  function testLimitLengthViewwithUrl(){
 
-    var noteListView = new NoteListView(list);
+    function Notedouble2(){};
+    Notedouble2.prototype.getText = function(){return "Favourite food: pesto"};
+    Notedouble2.prototype.getId = function(){return 2};
+    note2 = new Notedouble2();
+
+    function Notedouble1(){};
+    Notedouble1.prototype.getText = function(){return "Favourite drink: seltzer"};
+    Notedouble1.prototype.getId = function(){return 3};
+    note3 = new Notedouble1();
+
+    notelist = [note2, note3]
+
+
+    var display = '<ul><li><div><a herf="http://localhost:8080#notes/2">Favourite food: pest</a></div></li><li><div><a herf="http://localhost:8080#notes/3">Favourite drink: sel</a></div></li></ul>';
+
+    var noteListView = new NoteListView(notelist);
     assert.isTrue(noteListView.get_view() === display)
   }
-    testLimitLengthView()
-    exports.testLimitLengthView = testLimitLengthView;
+    testLimitLengthViewwithUrl()
+    exports.testLimitLengthViewwithUrl = testLimitLengthViewwithUrl;
 })(this);
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,19 +1,27 @@
 (function(exports){
-  function NoteListView(list){this.list = list};
+  function NoteListView(notelist){
+    this.list = notelist
+  };
 
   NoteListView.prototype.get_view = function(){
+
       var string = "";
+
       this.list.map(function(item){
         var limtedLengthStr;
+        var noteText = item.getText();
+        var noteId = item.getId();
 
-        limtedLengthStr = item.length > 20? item.substr(0,20) : item;
+        limtedLengthStr = noteText.length > 20? noteText.substr(0,20) : noteText;
 
 
-        string = string + "<li><div>" + limtedLengthStr + "</div></li>"
+        string = string + "<li><div>" + `<a herf="http://localhost:8080#notes/${noteId}">` + limtedLengthStr + "</a>" + "</div></li>"
       })
+
       var result = "<ul>" + string + "</ul>";
       return result;
     }
+
 
 
 exports.NoteListView = NoteListView;
