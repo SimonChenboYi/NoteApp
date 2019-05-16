288eaacf9681dea27f87fa17dffd027b7b044376
Chenbo Yi <yichenbo@outlook.com>
2019-05-16 10:39:35 +0100

    implement note id

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,9 +1,21 @@
 (function(exports){
-  function testNote(){
-    const note = new Note('I 😍 python');
+  function testNoteGetText(){
+    var note = new Note('I 😍 python');
 
     assert.isTrue(note.getText() === 'I 😍 python')
   };
-  testNote();
-  exports.testNote = testNote;
+  testNoteGetText();
+  exports.testNoteGetText = testNoteGetText;
+})(this);
+
+(function(exports){
+  function testNoteGetId(){
+    var note0 = new Note('Happy Coding!');
+    var note1 = new Note('Are you having fun?');
+
+    assert.isTrue(note0.getId() + 1 === note1.getId());
+
+  };
+  testNoteGetId();
+  exports.testNoteGetId = testNoteGetId;
 })(this);
--- src/note-model.js
+++ src/note-model.js
@@ -1,6 +1,11 @@
 (function(exports){
-  function Note(text){this.text = text}
+  var id = 0;
+  function Note(text){this.text = text; this.id = id++;};
 
-  Note.prototype.getText = function(){ return this.text }
+  Note.prototype.getText = function(){ return this.text };
+
+  Note.prototype.getId  = function(){return this.id};
+
+
   exports.Note = Note;
 })(this);
